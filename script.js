5a5996a7f9214feba988e3a2c0175b4f3437d163
lexslie <lesliiee727@gmail.com>
2023-05-30 18:53:51 -0700

    header css completed
    remaining css and html in progress

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-var APIKey = "1315bf372e314ca89e141b1ab0e182ba";
+var weatherAPIKey = "1315bf372e314ca89e141b1ab0e182ba";
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var weatherqueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
